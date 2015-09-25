880e966c771fe7ca2365b023aaa25fe22cb2b632
dimgrek <dimgrek@gmail.com>
2015-09-25 13:17:43 +0300

    Initial commit

---
+++ ContacListXamarin/ContacListXamarin/App.cs
@@ -0,0 +1,32 @@
+﻿using ContacListXamarin.Views;
+using Xamarin.Forms;
+
+namespace ContacListXamarin
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new ContentPage
+            {
+                Content = new NavigationPage(new ContactListPage());
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ ContacListXamarin/ContacListXamarin/Views/ContactListPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ContacListXamarin.Views
+{
+    public partial class ContactListPage : ContentPage
+    {
+        public ContactListPage()
+        {
+            InitializeComponent();
+            addContactBtn.Clicked += OnAddThingsButtonClicked;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            var items = _thingsService.GetThings();
+            ThingsILoveList.ItemsSource = items;
+        }
+
+
+        async void OnAddThingsButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddContactPage());
+        }
+    
+    }
+}
